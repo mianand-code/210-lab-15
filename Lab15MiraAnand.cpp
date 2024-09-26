05c20bc713e07fb0f270847461f1367d5719e85b
Mira Anand <miraanand@comcast.net>
2024-09-26 14:46:15 -0700

    2nd commit

--- Lab15MiraAnand.cpp
+++ Lab15MiraAnand.cpp
@@ -2,6 +2,32 @@
 // Module 5, Lesson: OOP Introduction/Review, Assignment: Movie Class
 // Coded using Visual Studio Code for Mac
 
+#include <fstream> // to be able to read data from an external .txt file
+#include <iostream>
+#include <string>
+#include <vector> // // to use STD::vector
+using namespace std;
+
+// creation of Movie class
+class Movie
+{
+    // private member variables
+    private:
+        string movieTitle;
+        int yearReleased;
+        string screenwriterName;
+    // public member functions
+    // using inline functions for setters and getters, since functions are 1 line
+    public:
+    // creating all setters together, to keep it organized
+    // function header
+    // DESCRIPTION:
+    // ARGUMENTS:
+    // RETURNS:
+    void setTitle(string t)     { movieTitle = t; }
+    void setYear(int y)         { yearReleased = y; }
+};
+
 int main()
 {
     return 0;
